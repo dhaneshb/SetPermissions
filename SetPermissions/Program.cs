0f6216dcf9c2fdd7c0737ea705be52a848802bf6
dhaneshb <db@evision.dk>
2019-09-26 22:51:07 +0200

    Add project files.

---
+++ SetPermissions/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Security.AccessControl;
+using System.Security.Principal;
+using System.ServiceProcess;
+
+namespace SetPermissions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SetServicePermissions("RabbitMQ", "TestUser");
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Interoperability", "CA1404:CallGetLastErrorImmediatelyAfterPInvoke")]
+        public static void SetServicePermissions(string service, string username)
+        {
+            System.ServiceProcess.ServiceController sc = new System.ServiceProcess.ServiceController(service);
+            ServiceControllerStatus status = sc.Status;
+            
+            byte[] psd = new byte[0];
+            uint bufSizeNeeded;
+            bool ok = ServiceAccountHelper.QueryServiceObjectSecurity(sc.ServiceHandle, SecurityInfos.DiscretionaryAcl, psd, 0, out bufSizeNeeded);
+            if (!ok)
+            {
+                int err = Marshal.GetLastWin32Error();
+                if (err == 122 || err == 0)
+                { // ERROR_INSUFFICIENT_BUFFER
+                  // expected; now we know bufsize
+                    psd = new byte[bufSizeNeeded];
+                    ok = ServiceAccountHelper.QueryServiceObjectSecurity(sc.ServiceHandle, SecurityInfos.DiscretionaryAcl, psd, bufSizeNeeded, out bufSizeNeeded);
+                }
+                else
+                {
+                    throw new ApplicationException("error calling QueryServiceObjectSecurity() to get DACL for " + username + ": error code=" + err);
+                }
+            }
+            if (!ok)
+                throw new ApplicationException("error calling QueryServiceObjectSecurity(2) to get DACL for " + username + ": error code=" + Marshal.GetLastWin32Error());
+
+            // get security descriptor via raw into DACL form so ACE
+            // ordering checks are done for us.
+            RawSecurityDescriptor rsd = new RawSecurityDescriptor(psd, 0);
+            RawAcl racl = rsd.DiscretionaryAcl;
+            DiscretionaryAcl dacl = new DiscretionaryAcl(false, false, racl);
+
+            // Add start/stop/read access
+            NTAccount acct = new NTAccount(username);
+            
+            SecurityIdentifier sid = (SecurityIdentifier)acct.Translate(typeof(SecurityIdentifier));
+         
+            dacl.AddAccess(AccessControlType.Allow, sid, (int)ServiceAccountHelper.SERVICE_ACCESS.SERVICE_START, InheritanceFlags.None, PropagationFlags.None);
+            dacl.AddAccess(AccessControlType.Allow, sid, (int)ServiceAccountHelper.SERVICE_ACCESS.SERVICE_STOP, InheritanceFlags.None, PropagationFlags.None);
+
+            // convert discretionary ACL back to raw form; looks like via byte[] is only way
+            byte[] rawdacl = new byte[dacl.BinaryLength];
+            dacl.GetBinaryForm(rawdacl, 0);
+            rsd.DiscretionaryAcl = new RawAcl(rawdacl, 0);
+
+            // set raw security descriptor on service again
+            byte[] rawsd = new byte[rsd.BinaryLength];
+            rsd.GetBinaryForm(rawsd, 0);
+            ok = ServiceAccountHelper.SetServiceObjectSecurity(sc.ServiceHandle, SecurityInfos.DiscretionaryAcl, rawsd);
+            if (!ok)
+            {
+                throw new ApplicationException("error calling SetServiceObjectSecurity(); error code=" + Marshal.GetLastWin32Error());
+            }
+        }
+
+    }
+}
---
+++ SetPermissions/ServiceAccountHelper.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace SetPermissions
+{
+    public static class ServiceAccountHelper
+    {
+        [Flags]
+        public enum SERVICE_ACCESS : uint
+        {
+            STANDARD_RIGHTS_REQUIRED = 0xF0000,
+            SERVICE_QUERY_CONFIG = 0x00001,
+            SERVICE_CHANGE_CONFIG = 0x00002,
+            SERVICE_QUERY_STATUS = 0x00004,
+            SERVICE_ENUMERATE_DEPENDENTS = 0x00008,
+            SERVICE_START = 0x00010,
+            SERVICE_STOP = 0x00020,
+            SERVICE_PAUSE_CONTINUE = 0x00040,
+            SERVICE_INTERROGATE = 0x00080,
+            SERVICE_USER_DEFINED_CONTROL = 0x00100,
+            SERVICE_ALL_ACCESS = (STANDARD_RIGHTS_REQUIRED |
+                              SERVICE_QUERY_CONFIG |
+                              SERVICE_CHANGE_CONFIG |
+                              SERVICE_QUERY_STATUS |
+                              SERVICE_ENUMERATE_DEPENDENTS |
+                              SERVICE_START |
+                              SERVICE_STOP |
+                              SERVICE_PAUSE_CONTINUE |
+                              SERVICE_INTERROGATE |
+                              SERVICE_USER_DEFINED_CONTROL)
+        }
+        [StructLayoutAttribute(LayoutKind.Sequential)]
+       public struct SECURITY_DESCRIPTOR
+        {
+            public byte revision;
+            public byte size;
+            public short control;
+            private IntPtr owner;
+            private IntPtr group;
+            private IntPtr sacl;
+            private IntPtr dacl;
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1060:MovePInvokesToNativeMethodsClass")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Interoperability", "CA1401:PInvokesShouldNotBeVisible")]
+        [DllImport("advapi32.dll", SetLastError = true)]
+       public static extern bool QueryServiceObjectSecurity(IntPtr serviceHandle,
+            System.Security.AccessControl.SecurityInfos secInfo,
+            ref SECURITY_DESCRIPTOR lpSecDesrBuf,
+            uint bufSize,
+            out uint bufSizeNeeded);
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1060:MovePInvokesToNativeMethodsClass")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Interoperability", "CA1401:PInvokesShouldNotBeVisible")]
+        [DllImport("advapi32.dll", SetLastError = true)]
+       public static extern bool QueryServiceObjectSecurity(SafeHandle serviceHandle,
+            System.Security.AccessControl.SecurityInfos secInfo,
+            byte[] lpSecDesrBuf,
+            uint bufSize,
+            out uint bufSizeNeeded);
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Interoperability", "CA1401:PInvokesShouldNotBeVisible")]
+        [DllImport("advapi32.dll", SetLastError = true)]
+       public static extern bool SetServiceObjectSecurity(SafeHandle serviceHandle,
+            System.Security.AccessControl.SecurityInfos secInfos,
+            byte[] lpSecDesrBuf);
+    }
+}
